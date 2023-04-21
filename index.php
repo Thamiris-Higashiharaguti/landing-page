c5bf5eb79247b7a79fe266045b0f6a23519d83a2
Thamiris-Higashiharaguti <thamirissayurihigashiharaguti@gmail.com>
2023-04-21 17:58:19 -0300

    Início do Header

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page Coffee Shop</title>
+
+    <!-- Bootstrap -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Custom CSS  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <!-- Início Header -->
+    <header class="header fixed-top">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-5">
+                    <a href="#home" class="logo">Coffee<span>Shop</span></a>
+                </div>
+                <div class="col">
+                    <nav>
+                        <a href="#home">Home</a>
+                        <a href="#sobre">Sobre nós</a>
+                        <a href="#parceiros">Nossos Parceiros</a>
+                        <a href="#localidades">Localidades</a>
+                        <a href="#contatos">Contatos</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Fim Header -->
+
+    <!-- Início Home -->
+    <!-- Fim Home -->
+
+    <!-- Início Form -->
+    <!-- Fim Form -->
+
+    <!-- Início Footer -->
+    <!-- Fim Footer -->
+
+
+    <!-- Custom CSS  -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
